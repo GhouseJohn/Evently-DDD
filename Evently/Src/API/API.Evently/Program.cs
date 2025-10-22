1020017b4512471de5a8add26e9d206a49f9ce87
GhouseJohn <md.gosoddin@gmail.com>
2025-10-22 12:19:55 +0530

    Moduler Mono Arch Designed Completd

---
+++ Evently/Src/API/API.Evently/Program.cs
@@ -0,0 +1,21 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapControllers();
+
+app.Run();
